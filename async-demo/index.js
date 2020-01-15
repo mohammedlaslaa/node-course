553956616d85881743c1998484eafd4082cb0d91
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-01-15 18:20:15 +0100

    Course 81 Completed

--- async-demo/index.js
+++ async-demo/index.js
@@ -39,7 +39,6 @@
 //     getRepositories(user.gitHubUsername, getcommits);
 // }
 
-
 // function getcommits(repos) {
 //   console.log(repos);
 // }
@@ -80,20 +79,36 @@
 //   })
 // });
 
-getUser(1)
-  .then(user => getRepositories(user.gitHubUsername))
-  .then(repos => getCommits(repos[0]))
-  .then(commits => console.log('Commits', commits))
-  .catch(err => console.log('Error', err.message));
+// Promise-based approach
 
-console.log('After');
+// getUser(1)
+//   .then(user => getRepositories(user.gitHubUsername))
+//   .then(repos => getCommits(repos[0]))
+//   .then(commits => console.log("Commits", commits))
+//   .catch(err => console.log("Error", err.message));
+
+//Async await approach
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  }catch(err){
+    console.log('Error :', err.message)
+  }
+}
+
+displayCommits();
+
+console.log("After");
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
     // Kick off some async work
     setTimeout(() => {
-      console.log('Reading a user from a database...');
-      resolve({ id: id, gitHubUsername: 'mohammed' });
+      console.log("Reading a user from a database...");
+      resolve({ id: id, gitHubUsername: "mohammed" });
     }, 2000);
   });
 }
@@ -101,8 +116,9 @@ function getUser(id) {
 function getRepositories(username) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log('Calling GitHub API...');
-      resolve(['repo1', 'repo2', 'repo3']);
+      console.log("Calling GitHub API...");
+      resolve(["repo1", "repo2", "repo3"]);
+      // reject(new Error('Could not get the repos.'))
     }, 2000);
   });
 }
@@ -110,7 +126,7 @@ function getRepositories(username) {
 function getCommits(repo) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log('Calling GitHub API...');
+      console.log("Calling GitHub API...");
       resolve(repo);
     }, 2000);
   });
