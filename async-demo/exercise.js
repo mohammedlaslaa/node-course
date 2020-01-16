ae42049d0b10ff8bdecc0f2f87d601313c5fbf68
Laslaa <mohamed.laslaa@gmail.com>
2020-01-16 09:50:08 +0100

    Exercise.js finished

--- async-demo/exercise.js
+++ async-demo/exercise.js
@@ -2,41 +2,63 @@ let obji = {
   id: 1,
   name: "Mohammed",
   isGold: true,
-  email: "email"
+  email: "mohamed.laslaa@gmail.com"
 };
 
-getCustomer(obji, customer => {
-  console.log("Customer: ", customer);
-  if (customer.isGold) {
-    getTopMovies(movies => {
-      console.log("Top movies: ", movies);
-      sendEmail(customer.email, movies, () => {
-        console.log("Email sent...");
-      });
-    });
+// getCustomer(obji, customer => {
+//   console.log("Customer: ", customer);
+//   if (customer.isGold) {
+//     getTopMovies(movies => {
+//       console.log("Top movies: ", movies);
+//       sendEmail(customer.email, movies, () => {
+//         console.log("Email sent...");
+//       });
+//     });
+//   }
+// });
+
+const mymovies = ["Movie1", "Movie2", "Movie3"]
+
+async function senMail() {
+  try {
+    const customer = await getCustomer(obji)
+    console.log("Customer : ", customer);
+    if (customer.isGold) {
+      const movies = await getTopMovies(mymovies);
+      console.log(movies);
+      const sendmail = await sendEmail(customer.email, movies);
+      console.log(sendmail);
+    }
+  } catch (err) {
+    console.log('Error :', err.message);
   }
-});
-
-async senMail(){
-  const customer = await getCustomer(obji)
 }
 
-function getCustomer(obj, callback) {
-  setTimeout(() => {
-    callback(obj);
-  }, 4000);
+senMail();
+
+function getCustomer(obj) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(obj);
+    }, 4000)
+  })
+
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(["movie1", "movie2"]);
-  }, 4000);
+function getTopMovies(movies) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(movies);
+    }, 4000);
+  })
 }
 
-function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+function sendEmail(email, movies) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(`Email sent to ${email}`);
+    }, 4000);
+  })
 }
 // TEST CALLBACK PERSO
 // function log1 (callback){
