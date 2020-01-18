1b3d3d966920d856acdff3b78215c1c4c74314a4
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-01-18 13:54:04 +0100

    Section 7 completed

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -42,4 +42,69 @@ async function getCourse() {
   console.log(courses);
 }
 
-getCourse();
+
+// Updating document
+// first approach : query first
+// findById()
+// Modify properties
+// save()
+
+// async function updateCourse(id){
+// 
+//   const course = await Course.findById(id);
+//   if(!course) return console.log('no courses found');
+
+//   course.isPublished = false;
+//   course.author = 'Mohammed';
+
+//   // Or
+//   // course.set({
+//   //   isPublished : true,
+//   //   author : 'Another Author'
+//   // })
+ 
+//   const result = await course.save();
+//   console.log(result)
+//   
+// }
+
+// second approach : update first
+// Updtae directly
+// optionally : get the updated document
+
+async function updateCourse(id){
+  // const result = await Course.update({_id : id}, {
+  // const result = await Course.update({_id : id}, {
+  //   $set : {
+  //     author : 'Mosh',
+  //     isPublished : false
+  //   }
+  // });
+  // console.log(result)
+  // you can also update and return the original document with 
+  // findByIdAndUpdate()
+  const course = await Course.findByIdAndUpdate(id, {
+    $set : {
+      author : 'Mohammed',
+      isPublished : false
+    }
+  }, {new : true}) // By this it will return the new document
+  // that we are updating;
+  console.log(course)
+}
+
+async function removeCourse(id){
+  // Delete only one or the first with a given property
+  const result = await Course.deleteOne({_id : id});
+  console.log(result)
+  // Delete only one or the first with a given property
+  // const result = await Course.deleteMany({/*Property}*/);
+  // find the document with a id and return the document which 
+  //has been deleted
+  // const course = await Course.findByIdAndRemove(id);
+  // console.log(course) // null if the document does not exist
+  
+}
+
+
+removeCourse('5e22fd3eb59d8f2434039c40');
