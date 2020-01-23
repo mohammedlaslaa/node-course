80d8b8cf41445a716f6fede40c0663a094a128a7
Laslaa <mohamed.laslaa@gmail.com>
2020-01-23 13:07:00 +0100

    2301202013h

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -2,6 +2,7 @@ const { User, validateUser } = require("../models/userModel");
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const _ = require('lodash');
 
 mongoose.set("useFindAndModify", false);
 
@@ -10,20 +11,17 @@ router.post('/', async (req, res) => {
     const { error } = validateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({email : req.body.email});
+    let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
+    user = new User(_.pick(req.body, ['name', 'email', 'password']))
     try {
         await user.save();
-        res.send(user)
+        _.pick(user, ['name', 'email']);
+        res.send(_.pick(user, ['_id', 'name', 'email']))
     } catch (err) {
-        console.log(err.message)
-    }
+    console.log(err.message)
+}
 })
 
 module.exports = router;
