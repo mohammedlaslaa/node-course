3052a150100549ff643b1f2cd571d9a9bbafeb48
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-01-28 22:39:12 +0100

    Bug fixes

--- vidly/index.js
+++ vidly/index.js
@@ -2,15 +2,18 @@ const winston = require("winston");
 const express = require("express");
 const app = express();
 
+
 require("./startup/routes")(app);
 require("./startup/database")();
-require("./startup/logging")();
+require("./startup/logging")(); 
 require("./startup/config")();
-require("./startup/validation")();
+require("./startup/validation")(); 
+require("./startup/prod")(app);
 
-const port = process.env.PORT || 9030;
+const port = process.env.PORT || 8010;
 
 const server = app.listen(port, () => {
+  console.log('ulyeeee')
   winston.info(`Server listening to the port ${port}`);
 });
  
